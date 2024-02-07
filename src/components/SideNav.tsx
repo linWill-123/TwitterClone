1e368680f3260c7608d994d02df1ef29c5f15e99
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-06 21:52:05 -0800

    hover effect and icons for nav

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,5 +1,7 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
+import { IconHoverEffect } from "./IconHoverEffect";
+import { VscAccount, VscHome, VscSignIn, VscSignOut } from "react-icons/vsc";
 
 export function SideNav() {
     const session = useSession();
@@ -8,20 +10,48 @@ export function SideNav() {
     return <nav className="sticky top-0 px-2 py-4">
         <ul className="flex flex-col items-start gap-2 whitespace-nowrap">
             <li>
-                <Link href="/">Home</Link>
+                <Link href="/">
+                    <IconHoverEffect>
+                        <span className="flex items-center gap-4">
+                            <VscHome className="h-8 w-8" />
+                            <span className="hidden text-lg md:inline">Home</span>
+                        </span>
+                    </IconHoverEffect>
+                </Link>
             </li>
             {user != null && (
                 <li>
-                    <Link href={`/profiles/${user.id}`}>Profile</Link>
+                    <Link href={`/profiles/${user.id}`}>
+                    <IconHoverEffect>
+                        <span className="flex items-center gap-4">
+                            <VscAccount className="h-8 w-8" />
+                            <span className="hidden text-lg md:inline">Profile</span>
+                        </span>
+                    </IconHoverEffect>
+                    </Link>
                 </li>
             )}
             {user == null ? (
                 <li>
-                    <button onClick={() => void signIn()}>Log In</button>
+                    <button onClick={() => void signIn()}>
+                        <IconHoverEffect>
+                            <span className="flex items-center gap-4">
+                                <VscSignIn className="h-8 w-8 fill-green-700" />
+                                <span className="hidden text-lg text-green-700 md:inline">log In</span>
+                            </span>
+                        </IconHoverEffect>
+                    </button>
                 </li>
             ) : (
                 <li>
-                    <button onClick={() => void signOut()}>Log Out</button>
+                    <button onClick={() => void signOut()}>
+                        <IconHoverEffect>
+                            <span className="flex items-center gap-4">
+                                <VscSignOut className="h-8 w-8 fill-red-700" />
+                                <span className="hidden text-lg text-red-700 md:inline">log Out</span>
+                            </span>
+                        </IconHoverEffect>
+                    </button>
                 </li>
             )} 
             
