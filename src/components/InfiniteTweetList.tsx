c6d07fcf96da502e5b705570719ce2b6278e28b4
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-06 21:45:02 -0800

    like botton style

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -2,6 +2,10 @@ import { isError } from "@tanstack/react-query"
 import InfiniteScroll  from "react-infinite-scroll-component"
 import { ProfileImage } from "./ProfileImage";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { VscHeart, VscHeartFilled } from "react-icons/vsc"
+import { IconHoverEffect } from "./IconHoverEffect";
+
 type Tweet = {
     id: string,
     content: string,
@@ -57,12 +61,40 @@ function TweetCard({id, user, content, createdAt, likeCount, likeByMe}: Tweet){
                         <span className="text-gray-500">{dateTimeFormatter.format()}</span>
                 </div>
                 {/* content */}
+                
                 <p className="whitespace-pre-wrap">{content}</p>
-                <HeartButton />
+                <HeartButton likedByMe={likeByMe} likeCount={likeCount}/>
             </div>
         </li>
 }
 
-function HeartButton() {
-    return <h1>Heart</h1>
+type HeartButtonProps = {
+    likedByMe: boolean,
+    likeCount: number
+}
+
+function HeartButton( {likedByMe, likeCount}: HeartButtonProps ) {
+    // check if user has logged in to be able to like a tweet
+    const session = useSession();
+    const HeartIcon = likedByMe ? VscHeartFilled : VscHeart;
+    
+    if (session.status !== "authenticated") {
+        return <div className="mb-1 mt-1 flex items-center gap-3 self-start text-gray-500">
+            <HeartIcon />
+            <span>{likeCount}</span>
+        </div>
+    }
+    return (
+        <button className={`group items-center gap-1 self-start flex transition-colors duration-200 
+                            ${likedByMe 
+                                ? "text-red-500" 
+                                : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"}`}>
+            <IconHoverEffect red>
+            <HeartIcon className={`transition-colors duration-200 ${
+                likedByMe ? "fill-red-500" 
+                          : "group-hover:fill-red-500 fill-gray-500 group-focus-visible:fill-red-500"}`} />
+            </IconHoverEffect>
+            <span>{likeCount}</span>
+        </button>
+    )
 }
